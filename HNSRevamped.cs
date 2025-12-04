1033fef95e74e1c6b0b676462d321a627606ec66
Ape <155788660+ApeMV@users.noreply.github.com>
2025-12-04 17:39:46 +0100

    Project base

---
+++ HNSRevamped.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace HNSRevamped;
+
+[BepInAutoPlugin]
+[BepInProcess("Among Us.exe")]
+public partial class Main : BasePlugin
+{
+    public Harmony Harmony { get; } = new(Id);
+    public static BepInEx.Logging.ManualLogSource Logger;
+    public static BasePlugin Instance;
+
+
+    public override void Load()
+    {
+        var handler = HNSRevamped.Logger.Handler("GitVersion");        
+        Logger = BepInEx.Logging.Logger.CreateLogSource("HNSRevamped");
+        HNSRevamped.Logger.Enable();
+        Instance = this;
+
+        Harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(ModManager), nameof(ModManager.LateUpdate))]
+    class ModManagerLateUpdatePatch
+    {
+        public static void Prefix(ModManager __instance)
+        {
+            __instance.ShowModStamp();
+        }
+    }
+}
